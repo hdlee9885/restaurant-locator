321f498e1904f85ec7a1f74f8bc30e3698aabb75
Haosong Li <hdlee9885@hotmail.com>
2021-01-19 23:52:31 -0600

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello world</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+          <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
